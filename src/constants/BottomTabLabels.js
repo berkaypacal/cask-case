a900f440b929c8e17e02be9187fb7840054086ea
berkay.pacal <berkay.pacal@mtdt.co>
2025-04-30 23:37:01 +0300

    feat: Add constants for bottom tab labels and screen names

---
+++ src/constants/BottomTabLabels.js
@@ -0,0 +1,7 @@
+const BottomTabLabels = {
+  TAB_ONE: 'Tab 1',
+  TAB_TWO: 'Tab 2',
+  TAB_THREE: 'Tab 3',
+};
+
+export default BottomTabLabels;
---
+++ src/constants/ScreenNames.js
@@ -0,0 +1,11 @@
+const ScreenNames = {
+  DEFAULT_SCREEN: 'DefaultScreen',
+  TEXT_SCREEN: 'TextScreen',
+  IMAGE_SCREEN: 'ImageScreen',
+  VIDEO_SCREEN: 'VideoScreen',
+  TAB_ONE: 'TabOne',
+  TAB_TWO: 'TabTwo',
+  TAB_THREE: 'TabThree',
+};
+
+export default ScreenNames;
