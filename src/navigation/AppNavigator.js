9e0560f063440b8a02c25e494c08beadd372348f
berkay.pacal <berkay.pacal@mtdt.co>
2025-04-30 23:37:12 +0300

    feat: Refactor AppNavigator to use constants for screen names and improve tab bar styling

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -7,29 +7,56 @@ import TabThree from '../screens/TabThree';
 import TextScreen from '../screens/TabOne/TextScreen';
 import ImageScreen from '../screens/TabOne/ImageScreen';
 import VideoScreen from '../screens/TabOne/VideoScreen';
+import tabBarStyles from './AppNavigator.styles';
+import ScreenNames from '../constants/ScreenNames';
+import BottomTabLabels from '../constants/BottomTabLabels';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabOneStack = () => (
   <Stack.Navigator
-    initialRouteName="DefaultScreen"
+    initialRouteName={ScreenNames.DEFAULT_SCREEN}
     screenOptions={{headerShown: false}}>
-    <Stack.Screen name="DefaultScreen" component={DefaultScreen} />
-    <Stack.Screen name="TextScreen" component={TextScreen} />
-    <Stack.Screen name="ImageScreen" component={ImageScreen} />
-    <Stack.Screen name="VideoScreen" component={VideoScreen} />
+    <Stack.Screen name={ScreenNames.DEFAULT_SCREEN} component={DefaultScreen} />
+    <Stack.Screen name={ScreenNames.TEXT_SCREEN} component={TextScreen} />
+    <Stack.Screen name={ScreenNames.IMAGE_SCREEN} component={ImageScreen} />
+    <Stack.Screen name={ScreenNames.VIDEO_SCREEN} component={VideoScreen} />
   </Stack.Navigator>
 );
 
 const AppNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={{headerShown: false}}
-      initialRouteName="TabOne">
-      <Tab.Screen name="TabOne" component={TabOneStack} />
-      <Tab.Screen name="TabTwo" component={TabTwo} />
-      <Tab.Screen name="TabThree" component={TabThree} />
+      screenOptions={{
+        headerShown: false,
+        ...tabBarStyles,
+      }}
+      initialRouteName={ScreenNames.TAB_ONE}>
+      <Tab.Screen
+        name={ScreenNames.TAB_ONE}
+        component={TabOneStack}
+        options={{
+          tabBarLabel: BottomTabLabels.TAB_ONE,
+          tabBarIcon: () => null,
+        }}
+      />
+      <Tab.Screen
+        name={ScreenNames.TAB_TWO}
+        component={TabTwo}
+        options={{
+          tabBarLabel: BottomTabLabels.TAB_TWO,
+          tabBarIcon: () => null,
+        }}
+      />
+      <Tab.Screen
+        name={ScreenNames.TAB_THREE}
+        component={TabThree}
+        options={{
+          tabBarLabel: BottomTabLabels.TAB_THREE,
+          tabBarIcon: () => null,
+        }}
+      />
     </Tab.Navigator>
   );
 };
---
+++ src/navigation/AppNavigator.styles.js
@@ -0,0 +1,17 @@
+const tabBarStyles = {
+  tabBarStyle: {
+    backgroundColor: '#123B66',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    position: 'absolute',
+    height: 90,
+  },
+  tabBarLabelStyle: {
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  tabBarActiveTintColor: '#fff',
+  tabBarInactiveTintColor: 'rgba(255,255,255,0.5)',
+};
+
+export default tabBarStyles;
