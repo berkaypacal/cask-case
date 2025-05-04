c13a86ba23b8a62816c5258965a7197301f22849
berkay.pacal <berkay.pacal@mtdt.co>
2025-05-04 20:45:10 +0300

    feat: Implement TabTwo screen with layout, token display, message type picker, delay input, and send button

--- src/screens/TabTwo/index.js
+++ src/screens/TabTwo/index.js
@@ -1,11 +1,27 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import LayoutComponent from '../../components/base/LayoutComponent';
+import styles from './styles';
+import TokenDisplay from '../../components/base/TokenDisplay';
+import MessageTypePicker from '../../components/base/MessageTypePicker';
+import DelayInput from '../../components/base/DelayInput';
+import SendButton from '../../components/base/SendButton';
 
 const TabTwo = () => {
+  const [pnType, setPnType] = useState('');
+  const [delay, setDelay] = useState('');
+  const fcmToken =
+    'CASKvugnSMrRrQPfb5Ty6OOKZBX268QlVOkZKmhOXIwINP1zlBih2h5emfTApbGUsTS85jYbvvKGrbQzsI4TCujbGuwidySnnDEtdg7NdYS5CKYR2j7QF4kkslnYABGdIcnT';
+
   return (
-    <View>
-      <Text>TabTwo</Text>
-    </View>
+    <LayoutComponent title="Tab 2">
+      <View style={styles.container}>
+        <TokenDisplay token={fcmToken} />
+        <MessageTypePicker pnType={pnType} setPnType={setPnType} />
+        <DelayInput delay={delay} setDelay={setDelay} />
+        <SendButton />
+      </View>
+    </LayoutComponent>
   );
 };
 
